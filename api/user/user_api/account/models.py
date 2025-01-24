25613b1f3cb5c69e9cc37fb6fdc66388fb5fc452
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-01-24 22:49:38 +0400

    feat: Creating account app and its model for managing user account

---
+++ api/user/user_api/account/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from uuid import uuid4
+from user.models import User, AbstrasctModel
+
+
+class Account(AbstrasctModel):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        editable=False
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=False
+    )
+    bio = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name="Biography"
+    )
+    location = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name="Location"
+    )
+    profile_pic = models.ImageField(
+        upload_to="profile_pictures/",
+        null=True,
+        verbose_name="Profile Picture"
+    )
+    last_login = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Last Login"
+    )
+    social_links = models.JSONField(
+        null=True,
+        verbose_name="Social Links"
+    )
+
+    class Meta:
+        ordering = ["id"]
+
+
