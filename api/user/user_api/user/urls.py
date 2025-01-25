e53f94f63739d70e1c478508f1d23ed5bed15412
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-01-25 15:01:18 +0400

    fix: fixing url problem

--- api/user/user_api/user/urls.py
+++ api/user/user_api/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserViewSet
 
 urlpatterns = [
-    path('/my-profile', UserViewSet.as_view({'patch': 'get_user_info'})),
-    path('/my-profile/update', UserViewSet.as_view({ 'patch': 'change_user_info' })),
+    path('my-profile', UserViewSet.as_view({'patch': 'get_user_info'})),
+    path('my-profile/update', UserViewSet.as_view({ 'patch': 'change_user_info' })),
 ]
