39406aa68d66ac5210e3b200be103dd4893b1ad4
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-02-02 20:42:58 +0400

    feat: connect project to database

--- api/user/user_api/src/app.module.ts
+++ api/user/user_api/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
+import { config } from './orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/user/user_api/src/orm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const config: TypeOrmModuleOptions = {
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: String(process.env.DB_PASSWORD),
+    database: process.env.DB_NAME,
+    synchronize: true,
+    autoLoadEntities: true
+}
