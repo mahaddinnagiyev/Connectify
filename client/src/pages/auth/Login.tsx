810b42423a193e1f72adc26035e283e809a9c7ec
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-02-04 23:57:23 +0400

    feat: Creating login functions and apply it to the page

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -2,20 +2,71 @@ import "./login.css";
 import google_logo from "../../assets/google.png";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { login } from "../../services/auth/auth-service";
+import ErrorMessage from "../../components/messages/ErrorMessage";
+import SuccessMessage from "../../components/messages/SuccessMessage";
 
 const Login = () => {
   const getUrl = (params: string) => {
-    const url = window.location.href
+    const url = window.location.href;
 
     if (url.split("/").includes(params)) {
-        return true
+      return true;
     }
 
-    return false
+    return false;
+  };
+
+  const [formData, setFormdata] = useState({
+    username_or_email: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await login(formData);
+
+    if (response.success) {
+      window.location.replace("/");
+      setSuccessMessage("Logged in successfully!");
+    } else {
+      if (Array.isArray(response.message)) {
+        setErrorMessage(response.message[0]);
+      } else {
+        setErrorMessage(
+          response.response?.error ??
+            response.message ??
+            response.error ??
+            "Invalid username or password"
+        );
+      }
+    }
   };
 
   return (
     <>
+      {errorMessage && (
+        <ErrorMessage
+          message={errorMessage}
+          onClose={() => setErrorMessage(null)}
+        />
+      )}
+
+      {successMessage && (
+        <SuccessMessage
+          message={successMessage}
+          onClose={() => setSuccessMessage(null)}
+        />
+      )}
+
       <main id="auth-main">
         <section id="login">
           <div className="logo">
@@ -27,7 +78,12 @@ const Login = () => {
             <Link to="/auth/login" className={getUrl("login") ? "active" : ""}>
               Log in
             </Link>
-            <Link to="/auth/signup" className={getUrl("signup") ? "active" : ""}>Sign up</Link>
+            <Link
+              to="/auth/signup"
+              className={getUrl("signup") ? "active" : ""}
+            >
+              Sign up
+            </Link>
           </div>
 
           <div className="google-btn">
@@ -44,15 +100,16 @@ const Login = () => {
             </p>
           </div>
 
-          <form action="" method="POST" className="login-form">
+          <form onSubmit={handleSubmit} method="POST" className="login-form">
             <div className="login-form-group">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="username_or_email">Username or Email</label>
               <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
+                type="text"
+                id="username_or_email"
+                name="username_or_email"
+                placeholder="Enter your username or email"
                 required
+                onChange={handleChange}
               />
             </div>
 
@@ -64,6 +121,7 @@ const Login = () => {
                 name="password"
                 placeholder="Enter your password"
                 required
+                onChange={handleChange}
               />
             </div>
 
--- client/src/services/auth/auth-service.ts
+++ client/src/services/auth/auth-service.ts
@@ -1,9 +1,18 @@
 import { ConfirmAccountDTO } from "./dto/confirm-account-dto";
+import { LoginDTO } from "./dto/login-dto";
 import { SignupDTO } from "./dto/singup-dto";
 
 const SERVER_USER_URL = "http://localhost:3535";
 
-export const signup = async (user: SignupDTO): Promise<{ success?: boolean, message?: string, error?: string, response: { success: boolean, message?: string, error?: string } }> => {
+// Signup
+export const signup = async (
+  user: SignupDTO
+): Promise<{
+  success?: boolean;
+  message?: string;
+  error?: string;
+  response: { success: boolean; message?: string; error?: string };
+}> => {
   const response = await fetch(`${SERVER_USER_URL}/auth/signup`, {
     method: "POST",
     headers: {
@@ -17,7 +26,15 @@ export const signup = async (user: SignupDTO): Promise<{ success?: boolean, mess
   return data;
 };
 
-export const confirm_account = async (code: ConfirmAccountDTO ): Promise<{ response: { success: boolean,  error?: string, message?: string}, success?: boolean, message?: string, error?: string }> => {
+// Confirm Accont Via Email
+export const confirm_account = async (
+  code: ConfirmAccountDTO
+): Promise<{
+  response: { success: boolean; error?: string; message?: string };
+  success?: boolean;
+  message?: string;
+  error?: string;
+}> => {
   const response = await fetch(`${SERVER_USER_URL}/auth/signup/confirm`, {
     method: "POST",
     headers: {
@@ -28,5 +45,27 @@ export const confirm_account = async (code: ConfirmAccountDTO ): Promise<{ respo
   });
 
   const data = await response.json();
+  return data;
+};
+
+// Login
+export const login = async (
+  user: LoginDTO
+): Promise<{
+  success?: boolean;
+  message?: string;
+  error?: string;
+  response: { success: boolean; message?: string; error?: string };
+}> => {
+  const resposne = await fetch(`${SERVER_USER_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(user),
+    credentials: "include",
+  });
+
+  const data = await resposne.json();
   return data;
 };
