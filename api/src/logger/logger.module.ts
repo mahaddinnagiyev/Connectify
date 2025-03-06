70fd3d415e002f619fb8aa060e0ccd21e9396bf4
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-03-06 22:36:03 +0400

    refactor: change logger database from local pgSql to supabase

--- api/src/logger/logger.module.ts
+++ api/src/logger/logger.module.ts
@@ -3,11 +3,12 @@ import { LoggerService } from './logger.service';
 import { LoggerController } from './logger.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Logger } from 'src/entities/logger.entity';
+import { SupabaseModule } from 'src/supabase/supabase.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Logger])],
+  imports: [TypeOrmModule.forFeature([Logger]), SupabaseModule],
   providers: [LoggerService],
   controllers: [LoggerController],
-  exports: [LoggerService]
+  exports: [LoggerService],
 })
 export class LoggerModule {}
--- api/src/logger/logger.service.ts
+++ api/src/logger/logger.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Logger } from 'src/entities/logger.entity';
 import { LogLevels } from 'src/enums/log-levels.enum';
-import { Repository } from 'typeorm';
+import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Injectable()
 export class LoggerService {
   constructor(
-    @InjectRepository(Logger)
-    private loggerRepository: Repository<Logger>,
+    private readonly supabase: SupabaseService,
   ) {}
 
   private async log(
@@ -18,46 +15,84 @@ export class LoggerService {
     details?: string,
     stack?: string,
   ) {
-    const logger = this.loggerRepository.create({
+    await this.supabase.getClient().from('user_logs').insert({
       level: level,
       message: message,
       module: module,
       details: details,
       stack: stack,
     });
-
-    await this.loggerRepository.save(logger);
   }
 
-  async emerg(message: string, module: string, details?: string, stack?: string) {
+  async emerg(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.emerg, message, module, details, stack);
   }
 
-  async alert(message: string, module: string, details?: string, stack?: string) {
+  async alert(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.alert, message, module, details, stack);
   }
 
-  async crit(message: string, module: string, details?: string, stack?: string) {
+  async crit(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.crit, message, module, details, stack);
   }
 
-  async error(message: string, module: string, details?: string, stack?: string) {
+  async error(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.error, message, module, details, stack);
   }
 
-  async warn(message: string, module: string, details?: string, stack?: string) {
+  async warn(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.warn, message, module, details, stack);
   }
 
-  async notice(message: string, module: string, details?: string, stack?: string) {
+  async notice(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.notice, message, module, details, stack);
   }
 
-  async info(message: string, module: string, details?: string, stack?: string) {
+  async info(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.info, message, module, details, stack);
   }
 
-  async debug(message: string, module: string, details?: string, stack?: string) {
+  async debug(
+    message: string,
+    module: string,
+    details?: string,
+    stack?: string,
+  ) {
     return this.log(LogLevels.debug, message, module, details, stack);
   }
 }
