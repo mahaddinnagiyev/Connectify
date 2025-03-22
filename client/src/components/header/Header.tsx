828d20060aa4f52d08db34759de3ca389ec38aa5
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-03-22 23:41:04 +0400

    fix: fixing header benu click bug

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import logo from "../../assets/app-logo.png";
 import no_profile_photo from "../../assets/no-profile-photo.png";
@@ -27,10 +27,25 @@ const Header = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [profilePicture, setProfilePicture] = useState<string | null>(null);
   const [receivedRequestsCount, setReceivedRequestsCount] = useState(0);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
-  const toggleMenu = () => {
+  const toggleMenu = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   const handleLogout = async () => {
     setIsLoading(true);
@@ -92,7 +107,10 @@ const Header = () => {
         </div>
 
         {/* Navbar buttons */}
-        <div className={`navbar ${isMenuOpen ? "open" : ""} flex gap-8`}>
+        <div
+          ref={menuRef}
+          className={`navbar ${isMenuOpen ? "open" : ""} flex gap-8`}
+        >
           <a href="/chat">
             <ChatIcon className="mt-1" /> Messenger
           </a>
@@ -125,7 +143,13 @@ const Header = () => {
           {isMenuOpen && (
             <div className="profile-actions-burger flex flex-col gap-2">
               <a href="/user/my-profile">
-                <AccountBoxIcon /> View Profile
+                <img
+                  src={profilePicture ?? no_profile_photo}
+                  alt=""
+                  style={{ width: "25px", height: "25px", borderRadius: "50%" }}
+                  className="border-2 border-[var(--primary-color)]"
+                />{" "}
+                View Profile
               </a>
               <a href="/settings">
                 <SettingsIcon /> Settings
