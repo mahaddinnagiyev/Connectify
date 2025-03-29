3596c2f6fed72792b0efe10a65aa84f7af30c016
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-03-29 15:12:52 +0400

    fix: attempt-4 - to fix session bug

--- client/src/services/auth/token-service.ts
+++ client/src/services/auth/token-service.ts
@@ -1,15 +1,24 @@
-export const getTokenFromSession = async (): Promise<{ access_token: string } | null> => {
-  const response = await fetch(`${process.env.SERVER_USER_URL}/auth/session/token`, {
+export const getTokenFromSession = async (): Promise<{
+  access_token: string;
+} | null> => {
+  const response = await fetch(
+    `${process.env.SERVER_USER_URL}/auth/session/token`,
+    {
       method: "GET",
       headers: {
-          "Content-Type": "application/json; charset=utf-8",
+        "Content-Type": "application/json; charset=utf-8",
       },
       credentials: "include",
-  });
+    }
+  );
+  
+  if (response.status === 204) {
+    return null;
+  }
 
   const data = await response.json();
   return data;
-}
+};
 
 export const getToken = async (): Promise<string | null> => {
   const response = await getTokenFromSession();
